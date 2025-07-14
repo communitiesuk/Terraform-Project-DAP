fe174e9c01fb9a715b22662976bf0c75e381a99a
abdulladoha-ubds <abdulla.doha@ubdsdigital.com>
2025-07-14 14:46:20 +0100

    updates tf files

--- networking.tf
+++ networking.tf
@@ -1,20 +1,19 @@
-
 data "aws_vpc" "existing" {
   id = var.existing_vpc_id
 }
-
-
+ 
+ 
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = data.aws_vpc.existing.id
   cidr_block              = var.private_subnet_cidr
   availability_zone       = var.availability_zone
   map_public_ip_on_launch = false
-
+ 
   tags = {
     Name = "kali-private-subnet"
   }
 }
-
+ 
 # # (Optional) Get the Internet Gateway attached to the existing VPC
 # data "aws_internet_gateway" "existing_igw" {
 #   filter {
@@ -22,21 +21,21 @@ resource "aws_subnet" "private_subnet" {
 #     values = [var.existing_vpc_id]
 #   }
 # }
-
+ 
 # # (Optional) Create a route table and associate it with the subnet
 # resource "aws_route_table" "private_rt" {
 #   vpc_id = data.aws_vpc.existing.id
-
+ 
 #   route {
 #     cidr_block = "0.0.0.0/0"
 #     gateway_id = data.aws_internet_gateway.existing_igw.id
 #   }
-
+ 
 #   tags = {
 #     Name = "kali-private-rt"
 #   }
 # }
-
+ 
 # resource "aws_route_table_association" "private_rt_assoc" {
 #   subnet_id      = aws_subnet.private_subnet.id
 #   route_table_id = aws_route_table.private_rt.id
--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
 variable "aws_region" {}
-variable "vpc_cidr" {}
+/*variable "vpc_cidr" {}
 variable "private_subnet_cidr" {}
-variable "availability_zone" {}
+variable "availability_zone" {}*/
 variable "instance_type" {}
 variable "key_name" {}
 variable "kali_ami_id" {}
 variable "kali_instance_count" {}
-
+ 
 variable "existing_vpc_id" {
   description = "VPC ID of an existing VPC"
   type        = string
 }
-
+ 
 variable "private_subnet_cidr" {
   description = "CIDR block for the private subnet"
   type        = string
 }
-
+ 
 variable "availability_zone" {
   description = "AZ for subnet"
   type        = string
